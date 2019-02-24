38d75a55f915b7553c031bb350cbe1db653ae65e
mrehmanzafar <m.rehmanzafar@gmail.com>
2019-02-23 22:17:17 -0500

    Initial code.

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Results:
+
+Epochs = 1
+
+Test loss: 0.1042552263200283
+Test accuracy: 0.9837
---
+++ capsule.py
@@ -0,0 +1,76 @@
+from keras.layers import *
+
+# Squashing function from hinton's paper.
+def squash(x, axis=-1):
+    s_squared_norm = K.sum(K.square(x), axis, keepdims=True) + K.epsilon()
+    scale = K.sqrt(s_squared_norm) / (1 + s_squared_norm)
+    return scale * x
+
+class Capsule(Layer):
+    # A Capsule implementation with Pure Keras from Paper: https://arxiv.org/abs/1710.09829
+
+    def __init__(self,
+                 num_capsule,
+                 dim_capsule,
+                 routings=3,
+                 share_weights=True,
+                 activation='squash',
+                 **kwargs):
+        super(Capsule, self).__init__(**kwargs)
+        self.num_capsule = num_capsule
+        self.dim_capsule = dim_capsule
+        self.routings = routings
+        self.share_weights = share_weights
+
+        if activation == 'squash':
+            self.activation = squash
+        else:
+            self.activation = activations.get(activation)
+
+    def build(self, input_shape):
+        input_dim_capsule = input_shape[-1]
+        if self.share_weights:
+            self.kernel = self.add_weight(
+                name='capsule_kernel',
+                shape=(1, input_dim_capsule,
+                       self.num_capsule * self.dim_capsule),
+                initializer='glorot_uniform',
+                trainable=True)
+        else:
+            input_num_capsule = input_shape[-2]
+            self.kernel = self.add_weight(
+                name='capsule_kernel',
+                shape=(input_num_capsule, input_dim_capsule,
+                       self.num_capsule * self.dim_capsule),
+                initializer='glorot_uniform',
+                trainable=True)
+
+    def call(self, inputs):
+        # Following the routing algorithm from Hinton's paper,
+        # b = b + <u,v>.
+
+        if self.share_weights:
+            hat_inputs = K.conv1d(inputs, self.kernel)
+        else:
+            hat_inputs = K.local_conv1d(inputs, self.kernel, [1], [1])
+
+        batch_size = K.shape(inputs)[0]
+        input_num_capsule = K.shape(inputs)[1]
+        hat_inputs = K.reshape(hat_inputs,
+                               (batch_size, input_num_capsule,
+                                self.num_capsule, self.dim_capsule))
+        hat_inputs = K.permute_dimensions(hat_inputs, (0, 2, 1, 3))
+
+        b = K.zeros_like(hat_inputs[:, :, :, 0])
+        for i in range(self.routings):
+            c = K.softmax(b, 1)
+            out = self.activation(K.batch_dot(c, hat_inputs, [2, 2]))
+            if i < self.routings - 1:
+                b += K.batch_dot(out, hat_inputs, [2, 3])
+                if K.backend() == 'theano':
+                    out = K.sum(out, axis=1)
+
+        return out
+
+    def compute_output_shape(self, input_shape):
+        return (None, self.num_capsule, self.dim_capsule)
